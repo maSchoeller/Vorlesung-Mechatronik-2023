ba853220725c7f3885616f2f49c164935a8b012c
maSchoeller <git@marvinschoeller.de>
2023-02-02 15:54:16 +0100

    vorlesung 1

---
+++ Vorlesung_1/Battleship/Program.cs
@@ -0,0 +1,64 @@
+﻿
+for (int i = 0; i < 20; i++)
+{
+    var board = new char[][]
+    {
+    "~~~~~~~~~~".ToCharArray(),
+    "~~~~~~O~~~".ToCharArray(),
+    "~OO~~~O~~~".ToCharArray(),
+    "~~~~~~O~~~".ToCharArray(),
+    "~~~~~~~~O~".ToCharArray(),
+    "~~~~~~~~O~".ToCharArray(),
+    "~OOOO~~~~~".ToCharArray(),
+    "~~~~~~~~~~".ToCharArray(),
+    "~~~~OOO~~~".ToCharArray(),
+    "~~~~~~~~~~".ToCharArray(),
+    };
+
+    int fieldCount = 14;
+    int counter = 0;
+    while (fieldCount != 0)
+    {
+        ShootOnBoard();
+        //PrintFieldToConsole();
+        counter++;
+    }
+    Console.WriteLine("Counter: " + counter);
+    void ShootOnBoard()
+    {
+        var rnd = new Random();
+
+        var y = rnd.Next(0, 10);
+        var x = rnd.Next(0, 10);
+        while (board[y][x] is 'X' or '*')
+        {
+            y = rnd.Next(0, 10);
+            x = rnd.Next(0, 10);
+        }
+        if (board[y][x] is 'O')
+        {
+            fieldCount--;
+
+            board[y][x] = 'X';
+        }
+        else
+        {
+            board[y][x] = '*';
+        }
+    }
+
+    void PrintFieldToConsole()
+    {
+        Console.WriteLine();
+
+        foreach (var line in board)
+        {
+            foreach (var field in line)
+            {
+                Console.Write(field + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ Vorlesung_1/GetStarted/Program.cs
@@ -0,0 +1,58 @@
+﻿
+//Primitive Datentypen
+int i = 0;
+char c = 'c';
+double d = 3.0d;
+bool b = false;
+
+//String ist ein Referenztyp lebt am Head und nicht am Stack!
+string s = "test";
+
+//Int Array mit vereinfachter Initializierung
+int[] myInts = { 1, 2, 3, 4, 5 };
+
+// 2 dimensionales Array symetrisch
+int[,] array2 = new int[3, 3];
+
+// 2 dimensionales Array asymetrisch
+int[][] array = new int[6][];
+array[0] = new int[8];
+array[1] = new int[10];
+
+
+if (b)
+{
+    var color = Console.BackgroundColor;
+    Console.BackgroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine("b = true");
+    Console.BackgroundColor = color;
+}
+else
+{
+    Console.WriteLine("b != true");
+}
+
+
+// switch expression, für keine Statements aus sondern gibt einen Wert zurück
+string colorStr = Console.BackgroundColor switch
+{
+    ConsoleColor.Red => "Red",
+    _ => "unkown"
+};
+
+//Schleifen im Vergleich
+while (true)
+{
+    Console.WriteLine("test");
+    break;
+}
+for (int test = 0; test < myInts.Length; test++)
+{
+    Console.WriteLine(myInts[test]);
+}
+
+foreach (int item in myInts)
+{
+    //item = 0;
+    Console.WriteLine(item);
+}
